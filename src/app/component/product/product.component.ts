de6fdbc31ef91166e2ccffcf3d17b8ab30e3637d
serbaysarikaya <ssarikaya@ritma.com.tr>
2024-02-28 13:42:00 +0300

    spiner

--- src/app/component/product/product.component.ts
+++ src/app/component/product/product.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
+
+
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
+
 export class ProductComponent implements OnInit {
+  products: Product[] = [];
+  dataLoaded=false;
 
-  products:Product[]=[];
+  constructor(private productService: ProductService) { }
 
- constructor(){}
+  ngOnInit(): void {
+    console.log("init çalıştı");
+    this.getProducts();
+  }
 
-ngOnInit(): void {
+  getProducts() {
+      this.productService.getProducts().subscribe(response => {
+      this.products = response.data
+      this.dataLoaded=true;
+         });
 
-}
-
+  }
 }
