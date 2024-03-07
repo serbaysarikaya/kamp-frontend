34fddc34f74dedd8bcebb26002c046708963a25c
serbaysarikaya <ssarikaya@ritma.com.tr>
2024-03-07 12:25:57 +0300

    toastr

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,11 +9,14 @@ import { Product } from '../models/product';
 })
 export class ProductService {
 
-  apiUrl = "https://localhost:7197/api/Products/getall";
+  apiUrl = "https://localhost:7197/api";
 
   constructor(private httpClient: HttpClient) { }
 
-  getProducts():Observable<ListResponseModel<Product>> {
-    return this.httpClient.get<ListResponseModel<Product>> (this.apiUrl);
-   }
+  getProducts(): Observable<ListResponseModel<Product>> {
+    return this.httpClient.get<ListResponseModel<Product>>(`${this.apiUrl}/Products/getall`);
+  }
+  getProductsByCategory(categoryId: number): Observable<ListResponseModel<Product>> {
+    return this.httpClient.get<ListResponseModel<Product>>(`${this.apiUrl}/Products/getbycategory?categoryId=${categoryId}`);
+  }
 }
